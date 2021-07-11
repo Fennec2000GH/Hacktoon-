20f949bb9c0abc3ac2be8c4c0c166b518f28f657
Fennec2000 <>
2021-07-11 14:01:05 +0000

    finalized scripts

--- src/shell.py
+++ src/shell.py
@@ -1,15 +1,15 @@
 
-import emoji, os, sys, time
+import emoji, os, random, sys
 from pprint import pprint
 from pynput.keyboard import Key, Controller, Listener
 from subprocess import Popen, PIPE
 from termcolor import colored, cprint
 
-from nlp_map import emoji_commands, natural_language_commands, translate_emoji
+from nlp_map import emoji_commands, natural_language_commands, translate_emoji, predict_command
 
 # GLOBAL VARIABLES
 # stored collections
-aliases = dict()  # key-value pair is {alias: valid command}
+alias_dict = dict()  # key-value pair is {alias: valid command}
 command_history = list()
 output_history = list()
 
@@ -56,8 +56,8 @@ def alias(command: str, *args):
   Return:
     None
   """
-  if command not in emoji_commands and command not in natural_language_commands:
-    raise ValueError(f'The command \'{command}\' is not yet registered.')
+  # if command not in emoji_commands and command not in natural_language_commands:
+  #   raise ValueError(f'The command \'{command}\' is not yet registered.')
 
   for arg in set(args):
     alias_dict[arg] = command
@@ -79,8 +79,8 @@ def nlp_add_sent(command: str, sentence: str):
   natural_language_commands[command].insert(sentence)
 
 magic_commands = dict({
-  'alias': lambda input: alias(command=input.split()[1], input.split()[2:]),
-  'nlp_add_sent': lambda input: nlp_add_sent(command=input.split()[1], sentence=input.split()[2])
+  'alias': lambda parts: alias(parts[1], *parts[2:]),
+  'nlp_add_sent': lambda parts: nlp_add_sent(command=parts[1], sentence=parts[2])
 })
 
 # keyboard = Controller()
@@ -98,18 +98,36 @@ def on_release(key):
 while(True):
 
   # collect user input
-  user_input = input(emoji.emojize(string=':growing_heart:\t'))
+  user_input = input(emoji.emojize(string=':man_superhero:\t'))
   command_history.append(colored(text=user_input, color='red'))
   cprint(text=user_input, color='red')
 
-  user_input_formatted = None
+  user_input_formatted = user_input
+  
+  # catch potential magic commands
+  if user_input.startswith('%'):
+    parts = user_input.split()
+    magic_cmd = parts[0][1:]
+    pprint(magic_cmd)
+
+    if magic_cmd not in magic_commands:
+      raise ValueError(f'The magic command \'{magic_cmd}\' does not exist.')
+      continue
+  
+    magic_func = magic_commands[magic_cmd]
+    magic_func(parts)
+    continue
+
+  # execute aliases when possible
+  if user_input in alias_dict:
+    user_input_formatted = alias_dict[user_input]
 
   # determine whther original user input is already a valid command
   try:
-    pipe = Popen(args=user_input.split(), stdout=PIPE)
+    pipe = Popen(args=user_input_formatted.split(), stdout=PIPE)
     output = pipe.communicate()[0].decode(encoding='UTF-8').split()
     for line in output:
-      formatted_line = colored(text=emoji.emojize(string=':bow_and_arrow:\t' + line), color='magenta')
+      formatted_line = colored(text=emoji.emojize(string=':man_supervillain:\t' + line), color=random.choice(list(colors)))
       output_history.append(formatted_line)
       print(formatted_line)
     continue
@@ -119,17 +137,22 @@ while(True):
     user_input_formatted = ''.join(list([translate_emoji(emoji=char) if ord(char) > 127 else char for char in user_input]))
     pprint(user_input_formatted)
 
-
   # determine whether emoji-translated user input is a valid command
   try:
     pipe = Popen(args=user_input_formatted.split(), stdout=PIPE)
     output = pipe.communicate()[0].decode(encoding='UTF-8').split()
     for line in output:
-      formatted_line = colored(text=emoji.emojize(string=':bow_and_arrow:\t' + line), color='magenta')
+      formatted_line = colored(text=emoji.emojize(string=':man_supervillain:\t' + line), color=random.choice(list(colors)))
       output_history.append(formatted_line)
       print(formatted_line)
     continue
 
   except Exception as err:
     # convert natural language into nearest valid command
-    pprint('NLP time!')
+    predict_cmd = predict_command(sentence=user_input_formatted)
+    pipe = Popen(args=predict_cmd.split(), stdout=PIPE)
+    output = pipe.communicate()[0].decode(encoding='UTF-8').split()
+    for line in output:
+      formatted_line = colored(text=emoji.emojize(string=':man_supervillain:\t' + line), color=random.choice(list(colors)))
+      output_history.append(formatted_line)
+      print(formatted_line)

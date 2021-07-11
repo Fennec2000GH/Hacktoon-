152012a094297a36e2f3d6cb8ee40a6684fc2c07
Fennec2000 <>
2021-07-11 01:07:40 +0000

    translation from emoji to raw text commands

--- src/nlp_map.py
+++ src/nlp_map.py
@@ -3,17 +3,33 @@ import textblob as tb
 from pprint import pprint
 
 emoji_commands = dict({
-  'cat': set(['🐈', '🐱', '🙀', '😾', '😿', '😼', '😺', '😹', '😸', '😽', '😻']),
-  'cp': set(['📋']),
-  'free': set(['🆓']),
-  'head': set(['🔝', '🆙']),
-  'ls': set(['📝', '📜']),
-  'mkdir': set(['📂']),
-  'mv': set(['✂️', '🔪', '🗡️', ]),
-  'nano': set(['🖊️', '🖋️', '✒️', '✍️', ]),
-  'rm': set(['🧽', '🧹', '🧼', ]),
-  'ssh': set(['🐚', '🐌', '🐢', '🦪']),
-  'tail': set(['🔽', '⏬', '⬇️'])
+  'cat': frozenset(['🐈', '🐱', '🙀', '😾', '😿', '😼', '😺', '😹', '😸', '😽', '😻']),
+  'cd': frozenset(['🗄', '🗂']),
+  'cp': frozenset(['📋']),
+  'free': frozenset(['🆓']),
+  'head': frozenset(['🔝', '🆙']),
+  'ls': frozenset(['📝', '📜']),
+  'mkdir': frozenset(['📁','📂', '🖿']),
+  'mv': frozenset(['✂️', '🔪', '🗡️', ]),
+  'nano': frozenset(['🖊️', '🖋️', '✒️', '✍️', ]),
+  'rm': frozenset(['🧽', '🧹', '🧼', ]),
+  'ssh': frozenset(['🐚', '🐌', '🐢', '🦪']),
+  'tail': frozenset(['🔽', '⏬', '⬇️'])
 })
 
-pprint(emoji_commands)
+def translate_emoji(emoji: str):
+  """
+  Get valid command from emoji.
+
+  Parameters:
+    emoji (str): Emoji to interpret.
+
+  Returns:
+    str: Valid Linux command from emoji. Invalid emoji will return None.
+  """  
+  for key, value in emoji_commands.items():
+    if emoji in value:
+      # pprint(key)
+      return key
+
+  raise ValueError(f'The emoji \'{emoji}\' is not valid for any command.')
--- src/shell.py
+++ src/shell.py
@@ -5,7 +5,9 @@ from pynput.keyboard import Key, Controller, Listener
 from subprocess import Popen, PIPE
 from termcolor import colored, cprint
 
+from nlp_map import emoji_commands, translate_emoji
 # GLOBAL VARIABLES
+# termcolor options
 colors = set([
     'grey',
     'red',
@@ -37,6 +39,8 @@ attrs = set([
     'concealed'
 ])
 
+# stored collections
+aliases = dict()
 command_history = list()
 output_history = list()
 
@@ -51,14 +55,42 @@ def on_release(key):
 # with Listener(on_press=on_press) as listener:
 #   listener.join()
 
+# REPL loop
 while(True):
 
-    user_input = input(emoji.emojize(string=':growing_heart:\t'))
-    command_history.append(colored(text=user_input, color='red'))
+  # collect user input
+  user_input = input(emoji.emojize(string=':growing_heart:\t'))
+  command_history.append(colored(text=user_input, color='red'))
+  cprint(text=user_input, color='red')
 
+  user_input_formatted = None
+
+  # determine whther original user input is already a valid command
+  try:
     pipe = Popen(args=user_input.split(), stdout=PIPE)
     output = pipe.communicate()[0].decode(encoding='UTF-8').split()
     for line in output:
-        formatted_line = colored(text=emoji.emojize(string=':bow_and_arrow:\t' + line), color='magenta')
-        output_history.append(formatted_line)
-        print(formatted_line)
+      formatted_line = colored(text=emoji.emojize(string=':bow_and_arrow:\t' + line), color='magenta')
+      output_history.append(formatted_line)
+      print(formatted_line)
+    continue
+
+  except Exception as err:
+    # convert emojis into valid commands
+    user_input_formatted = ''.join(list([translate_emoji(emoji=char) if ord(char) > 127 else char for char in user_input]))
+    pprint(user_input_formatted)
+
+
+  # determine whether emoji-translated user input is a valid command
+  try:
+    pipe = Popen(args=user_input_formatted.split(), stdout=PIPE)
+    output = pipe.communicate()[0].decode(encoding='UTF-8').split()
+    for line in output:
+      formatted_line = colored(text=emoji.emojize(string=':bow_and_arrow:\t' + line), color='magenta')
+      output_history.append(formatted_line)
+      print(formatted_line)
+    continue
+
+  except Exception as err:
+    # convert natural language into nearest valid command
+    pprint('NLP time!')
